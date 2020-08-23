1a4ac9edbbb4128c6ae87b5288e6aebf15c7e2c7
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-08-23 15:57:46 -0300

    New beginning point

---
+++ src/components/GameCardSlider/mock.ts
@@ -0,0 +1,50 @@
+export default [
+  {
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 235,
+    promotionalPrice: 215
+  },
+  {
+    title: 'Population Zero',
+    slug: 'population-zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: 235,
+    promotionalPrice: 215
+  }
+]
---
+++ src/components/GameInfo/index.tsx
@@ -0,0 +1,40 @@
+import {
+  AddShoppingCart,
+  FavoriteBorder
+} from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+import Heading from 'components/Heading'
+import Ribbon from 'components/Ribbon'
+import formatPrice from 'utils/format-price'
+
+import * as S from './styles'
+
+export type GameInfoProps = {
+  title: string
+  description: string
+  price: number
+}
+
+const GameInfo = ({ title, description, price }: GameInfoProps) => (
+  <S.Wrapper>
+    <Heading color="black" lineBottom>
+      {title}
+    </Heading>
+
+    <Ribbon color="secondary">{formatPrice(price)}</Ribbon>
+
+    <S.Description>{description}</S.Description>
+
+    <S.ButtonsWrapper>
+      <Button icon={<AddShoppingCart />} size="large">
+        Add to cart
+      </Button>
+      <Button icon={<FavoriteBorder />} size="large" minimal>
+        Wishlist
+      </Button>
+    </S.ButtonsWrapper>
+  </S.Wrapper>
+)
+
+export default GameInfo
